d7fd03eb0c53439da21eb9fce353dd476b1535d5
Tassuo7 <123276938+Tassuo7@users.noreply.github.com>
2024-06-07 00:40:46 +0100

    implement model serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
@@ -13,12 +14,13 @@ def api_home(request, *args, **kwargs):
     """
     #if request.method != "POST":
     #    return Response({"detail": "GET not allowed"}, status=405)
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    if instance:
+        data = ProductSerializer(instance).data
+       # data = model_to_dict(instance, fields=['id', 'title', 'price', 'discount_price', "get_discount_val"])
       #  data = model_to_dict(model_data)
-      # *****************
+      # ***************** instance
     #    data['id'] = model_data.id
     #    data['title'] = model_data.title
     #    data['content'] = model_data.content
