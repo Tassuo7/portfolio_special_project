4a032fb83b4986e75dcb1839efec802069ecdcd9
Tassuo7 <123276938+Tassuo7@users.noreply.github.com>
2024-06-08 02:46:11 +0100

    Session Authentication & Permissions

--- py_client/create.py
+++ py_client/create.py
@@ -1,6 +1,8 @@
 import requests
 
 endpoint = "http://127.0.0.1:8000/api/products/" # http://localhost:8000/
+# http://localhost:8000/admin/
+# session -> post data
 
 data = {
     "title": "this is the title",
