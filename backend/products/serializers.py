b742643cf3f3a1a6f63aaa3e479951163680bbd9
Tassuo7 <123276938+Tassuo7@users.noreply.github.com>
2024-06-09 03:33:49 +0100

    URLs, Reverse, & Serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,12 +1,21 @@
-from rest_framework import serializers, viewsets
+from rest_framework import serializers #viewsets
+from rest_framework.reverse import reverse
 
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     discount = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field='pk'
+    )
+
     class Meta:
         model = Product
         fields = [
+            'url',
+            'edit_url',
             'pk',
             'title',
             'content',
@@ -14,6 +23,13 @@ class ProductSerializer(serializers.ModelSerializer):
             'discount_price',
             'discount',
         ]
+
+    def get_edit_url(self, obj):
+        # return f"api/v2/products/{obj.pk}/"
+        request = self.context.get('request') #self.request
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
 
     def get_discount(self, obj):
         if not hasattr(obj, 'id'):
